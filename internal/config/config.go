8dc19f23df9b5bb34ca9cb8eeab71e7822ce9227
Denis Beresnev <shanthi.bunch@gmail.com>
2025-11-27 19:44:29 +0300

    feat: added configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,30 +1,25 @@
 package config
 
-import (
-	"os"
-	"strconv"
-)
+import "time"
 
 type Config struct {
-	Port      string
-	JWTSecret string
+	Env    string     `mapstructure:"env" yaml:"env"`
+	Addr   string     `mapstructure:"addr" yaml:"addr"`
+	HTTP   HTTPConfig `mapstructure:"http" yaml:"http"`
+	Logger Logger     `mapstructure:"logger" yaml:"logger"`
 }
 
-func LoadConfig() *Config {
-	return &Config{
-		Port:      getEnv("PORT", "8080"),
-		JWTSecret: getEnv("JWT_SECRET", "super_secret_key_for_dev_only"),
-	}
+type HTTPConfig struct {
+	ReadTimeout    time.Duration `mapstructure:"read_timeout" yaml:"read_timeout"`
+	WriteTimeout   time.Duration `mapstructure:"write_timeout" yaml:"write_timeout"`
+	IdleTimeout    time.Duration `mapstructure:"idle_timeout" yaml:"idle_timeout"`
+	RequestTimeout time.Duration `mapstructure:"request_timeout" yaml:"request_timeout"`
 }
 
-func getEnv(key, fallback string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-	return fallback
-}
-
-func (c *Config) GetPortInt() int {
-	p, _ := strconv.Atoi(c.Port)
-	return p
+type Logger struct {
+	App          string `mapstructure:"app" yaml:"app"`
+	Level        string `mapstructure:"level" yaml:"level"`
+	Service      string `mapstructure:"service" yaml:"service"`
+	UDPAddress   string `mapstructure:"udp_address" yaml:"udp_address"`
+	EnableCaller bool   `mapstructure:"enable_caller" yaml:"enable_caller"`
 }
