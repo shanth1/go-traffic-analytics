11d80877736aa6e8287db138214ab522051925c8
Denis Beresnev <shanthi.bunch@gmail.com>
2025-12-14 18:54:13 +0300

    refactor: removed old version of frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from '@/app/Layout';
-import { CampaignsPage } from '@/pages/CampaignsPage';
-import { AnalyticsPage } from '@/pages/AnalyticsPage';
-import { ProfilePage } from '@/pages/ProfilePage';
-import { CreatePage } from '@/pages/CreatePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="campaigns" element={<CampaignsPage />} />
-          <Route path="analytics" element={<AnalyticsPage />} />
-          <Route path="profile" element={<ProfilePage />} />
-          <Route path="create" element={<CreatePage />} />
-        </Route>
+        <Route path="/" element={<div></div>}></Route>
       </Routes>
     </BrowserRouter>
   );
