e44044812f9b7f207e0c4e3a6e0d07ba28dab364
Denis Beresnev <shanthi.bunch@gmail.com>
2025-12-16 18:55:12 +0300

    fix: campaigns page

--- frontend/src/pages/campaigns/CampaignsPage.tsx
+++ frontend/src/pages/campaigns/CampaignsPage.tsx
@@ -89,7 +89,7 @@ export const CampaignsPage = () => {
           ))}
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
           {campaigns.map((camp) => (
             <CampaignCard key={camp.id} data={camp} />
           ))}
