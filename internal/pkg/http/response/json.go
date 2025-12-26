6814455e77255f932b98c1038d14d3eb92722eed
Denis Beresnev <shanthi.bunch@gmail.com>
2025-12-26 23:18:01 +0300

    feat: added helper response messages

--- internal/pkg/http/response/json.go
+++ internal/pkg/http/response/json.go
@@ -3,6 +3,8 @@ package response
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/shanth1/gotools/log"
 )
 
 type Envelope map[string]interface{}
@@ -21,4 +23,14 @@ type ErrorResponse struct {
 
 func Error(w http.ResponseWriter, status int, message string) {
 	JSON(w, status, Envelope{"error": message})
+}
+
+func ErrorWithLog(w http.ResponseWriter, r *http.Request, err error, status int, userMessage string) {
+	logger := log.FromContext(r.Context())
+	logger.Error().Err(err).Msg("request failed")
+	Error(w, status, userMessage)
+}
+
+func ServerError(w http.ResponseWriter, r *http.Request, err error) {
+	ErrorWithLog(w, r, err, http.StatusInternalServerError, "Internal server error")
 }
