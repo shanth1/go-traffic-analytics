4bf96aa4d53aba13758098f78ae3041934e73724
Denis Beresnev <shanthi.bunch@gmail.com>
2026-01-12 22:17:17 +0300

    feat: added redirect host env

---
+++ frontend/src/shared/config/index.ts
@@ -0,0 +1,11 @@
+export const REDIRECT_HOST =
+  import.meta.env.VITE_REDIRECT_HOST || window.location.origin;
+
+/**
+ * Helper to construct full short URL
+ */
+export const getShortLink = (slug: string) => {
+  // Remove trailing slash from host if present
+  const host = REDIRECT_HOST.replace(/\/$/, '');
+  return `${host}/${slug}`;
+};
